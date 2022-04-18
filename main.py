3d115ea7c492ee8780fc3b57a94610203264b7cd
CarlosBerken <89818727+CarlosBerken@users.noreply.github.com>
2022-04-18 01:05:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+''' Comentario
+Autor: Carlos
+Data: 12/12/21
+Versão: 1.0
+'''
+
+#meu primeiro codigo (Comentario)
+print("Ola")
+print('Tudo Bem ?')
+print('Que dia é hoje ?')
+#meu segundo codigo
+print('Domingo')
